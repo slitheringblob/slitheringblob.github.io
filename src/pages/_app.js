04169f9395c56e9ecabc47a5ab635eb15df8c94e
jaydeepganatra <jaydeep.g@thinkbridge.in>
2023-12-12 19:59:03 +0530

    bunch of cleanup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,7 @@ import { ThemeProvider } from "next-themes";
 
 export default function App({ Component, pageProps }) {
   return (
-  <ThemeProvider defaultTheme='light' attribute='class'>
+  <ThemeProvider defaultTheme='dark' attribute='class'>
     <Component {...pageProps} />
   </ThemeProvider> 
   )
