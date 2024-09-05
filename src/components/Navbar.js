e5166f17593eda310475bdec1826b243a60389a9
Jaydeep Ganatra <jaydeepganatra11@gmail.com>
2024-09-05 17:50:19 +0530

    updated for xs form factor

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -148,7 +148,7 @@ export default function Navbar() {
       </div>
       <div className="space-x-8 block md:hidden mt-4">
         <Link
-          href="/about"
+          href="/"
           className="text-base font-normal text-gray-600 dark:text-gray-300"
         >
           About
