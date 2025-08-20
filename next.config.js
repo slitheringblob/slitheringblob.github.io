1377bcd15f9abd9c0e0ffa9261e29392ed9a9e59
slitheringblob <jaydeepganatra11@gmail.com>
2025-08-20 15:02:03 +0000

    updated outfile config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
